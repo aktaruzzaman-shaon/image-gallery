6ad0479a3df4bdddf6d803c4e024aaaf74303f30
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-11-02 10:23:35 +0600

    Added image hover checkbox

--- src/Component/Image.jsx
+++ src/Component/Image.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-const Image = () => {
+const Image = ({ singleImage }) => {
+    
     return (
-        <div>
-            <p>This is image page</p>
+        <div className='group relative cursor-pointer items-center justify-center overflow-hidden '>
+            <div>
+                <img src={singleImage.img} alt="" className='h-46 w-30 object-contain ' />
+            </div>
+            <div class="absolute inset-1 flex translate-y-[110%] flex-col  transition-all duration-500 group-hover:translate-y-0">
+                <input type="checkbox" className="checkbox rounded red" onChange={handleCheck} />
+            </div>
         </div>
     );
 };
--- src/Component/Images.jsx
+++ src/Component/Images.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from './Image';
 import { useForm } from 'react-hook-form';
 
 const Images = () => {
 
-    const { register, handleSubmit } = useForm();
+    const [allImages, setAllImages] = useState([])
+
+    //load all gallery images
+    useEffect(() => {
+        fetch('http://localhost:5000/images')
+            .then(res => res.json())
+            .then(data => setAllImages(data))
+    }, [allImages])
+
+    // useform for getting image file
+    const { register, handleSubmit, reset } = useForm();
     const imageStorageKey = '1d685d0dc62621d6524a698642b092eb';
 
+    // for uploading image to imgbb server 
     const onSubmit = async data => {
         const image = data.image[0];
         const formData = new FormData();
@@ -17,21 +28,41 @@ const Images = () => {
             body: formData
         })
             .then(res => res.json())
-            .then(result=>{
-                if(result.success){
+            .then(result => {
+                if (result.success) {
                     const uploadImageUrl = result.data.url;
-                    const imageUrl = {img: uploadImageUrl}
-                }
-                
-            })
-            
+                    const imageUrl = { img: uploadImageUrl }
 
+                    // after successfully upload in imgbb server pass the imagurl to monogdb server
+                    fetch('http://localhost:5000/addImageUrl', {
+                        method: 'POST',
+                        headers: {
+                            "content-type": "application/json",
+                            authorization: 'bearer'
+                        },
+                        body: JSON.stringify(imageUrl)
+                    })
+                        .then(res => res.json())
+                        .then(inserted => {
+                            console.log('inserted')
+                            reset()
+                        })
+                }
+            })
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input type="file" {...register("image", { required: true })} className='input input-bordered' />
-        </form>
+        <div>
+            <div className='grid grid-flow-row grid-cols-3 gap-4  '>
+                {
+                    allImages.map((singleImage, index) => <Image singleImage={singleImage} key={index}></Image>)
+                }
+            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="file" {...register("image", { required: true })} className='input input-bordered' />
+                <input type="submit" className='btn' value="add" />
+            </form>
+        </div>
     );
 };
 
